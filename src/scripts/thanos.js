5d335a35bcb8e947c21a230bcaeb94308583b56c
Nataliia-Homyk324 <homyk324@gmail.com>
2024-06-03 19:24:09 +0300

    step-7

--- src/scripts/thanos.js
+++ src/scripts/thanos.js
@@ -1,5 +1,17 @@
 import { PATH_DB } from '../constants/contacts.js';
+import fs from 'node:fs/promises';
 
-export const thanos = async () => {};
+export const thanos = async () => {
+    try {
+        const contacts = await fs.readFile(PATH_DB, 'utf-8');
+
+        const newContacts = JSON.parse(contacts).filter(() => Math.random() > 0.5);
+
+        await fs.writeFile(PATH_DB, JSON.stringify(newContacts));
+        console.log('Thanos has snapped his fingers');
+    } catch (error) {
+        console.error(`Error processing the contacts:: ${error.message}`);
+    }
+};
 
 await thanos();
