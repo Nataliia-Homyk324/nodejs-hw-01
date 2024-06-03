6cdaa6b80620846646241bdbe3ce73fae85bc335
Nataliia-Homyk324 <homyk324@gmail.com>
2024-06-03 18:51:26 +0300

    step-5

--- src/scripts/countContacts.js
+++ src/scripts/countContacts.js
@@ -1,5 +1,13 @@
 import { PATH_DB } from '../constants/contacts.js';
+import fs from 'node:fs/promises';
 
-export const countContacts = async () => {};
+export const countContacts = async () => {
+     try {
+        const contacts = await fs.readFile(PATH_DB, 'utf-8');
+        return `the number of contacts in the array: ${JSON.parse(contacts).length}`;
+    } catch {
+        console.log("Failed to count contacts!");
+    }
+};
 
 console.log(await countContacts());
